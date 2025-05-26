318aedda61f2ca227ef0a4fc06682368019a4321
Mandour <mahmoudmandour200015@gmail.com>
2025-05-26 20:38:25 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "./Components/Sidebar/sidebar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 import Main from "./Components/Main/Main";
 function App() {
   return (
